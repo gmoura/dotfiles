8f5043b41607332b150862477fd08aea9713a22e
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2015-11-26 14:26:50 +0000

    Update install rules

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 
 DOTFILES_DIR=$(dirname $0)
 
-echo $DOTFILES_DIR;
+
 ## Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/vim/.vundle.vim" ~
-#
+
 ## Package managers & packages
 
-"$DOTFILES_DIR/install/vundle.sh"
+. "$DOTFILES_DIR/install/vundle.sh"
 
