55152c7b705a6a49f821c0f2cacf4251976d7fc3
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2015-11-26 17:29:03 +0000

    Tools for Ubuntu

---
+++ tools/ubuntu/intall.sh
@@ -0,0 +1,17 @@
+## Install man packages
+
+sudo apt-get install nodejs git ruby-full vim zsh curl wget 
+
+## Install NPM packages
+
+sudo npm install -g grunt grunt-cli gulp bower
+
+## Install Ruby gems
+
+sudo gem install compass
+
+## Oh My Zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s `which zsh` 
+
