632ed31bda86c04578564b8342c0690f7d38d7b1
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2015-11-26 17:30:35 +0000

    Check OS
    Check the OS and load an specific installer

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
-#!/usr/bin/env bash
+:x
 
 # Get current dir (so run this script from anywhere)
 
 DOTFILES_DIR=$(dirname $0)
+OS=$(uname -s)
+TOOLS="$DOTFILES/tools"
+
+# Check operation system
+
+if[ -z "$OSTYPE" ]
+  case "$OSTYPE" in 
+    linux*) 
+      . "$TOOLS/ubuntu/install.sh"
+      echo "Linux"
+    ;;
+
+    darwin*)
+      echo "Mac"
+    ;;
+
+    ms*)
+      echo "Win - MING"
+    ;;
+  esac
+fi
 
 
 ## Bunch of symlinks
